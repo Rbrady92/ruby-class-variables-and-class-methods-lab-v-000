17c29b45751f3a0b04cb367d18404c1516cf38ef
Ryan Brady <ryanbrady369@gmail.com>
2018-10-24 21:52:47 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,32 +1,48 @@
 class Song
 
-  @@count = 0 
+  @@count = 0
   @@artists = []
   @@genres = []
-  
+
   def initialize(name, artist, genre)
     @name = name
     @@count += 1
     @@artits << artist
     @@genres << genre
-  end 
+  end
 
   attr_accessor :name, :artist, :genre
-  
+
   def self.count
     @@count
-  end 
+  end
 
   def self.genres
-    copy = @@genres.uniq 
+    copy = @@genres.uniq
     copy
-  end 
+  end
 
   def self.artists
     copy = @@artists.uniq
     copy
+  end
+
+  def self.genre_count
+    list = {}
+    @@genres.each do |curr|
+      list.keys.include?(curr) ? list[curr]+= 1 : list[curr] = 1
+    end 
+    list 
   end 
 
-  
+  def self.artist_count
+    list = {}
+    @@artists.each do |curr|
+      list.keys.include?(curr) ? list[curr]+= 1 : list[curr] = 1
+    end 
+    list
+  end 
 
+end 
+       
     #need artist count and genre count objects
